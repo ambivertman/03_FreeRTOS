459d0ca482adce54009b8cb81d5cea58954d485f
ambi <forworkingliu@163.com>
2025-07-14 11:34:53 +0800

    通过中断实现Delay

---
+++ 14_DelayByTim_IT/User/main.c
@@ -0,0 +1,42 @@
+#include "stm32f10x.h"    
+#include "freertos.h"
+#include "task.h"
+#include "UART1.h" 
+#include "Delay.h"
+
+TaskHandle_t hand1, hand2;
+
+void Print_Task_Info(TaskHandle_t TaskHandle) {
+	TaskStatus_t xTaskDetails;
+	vTaskGetInfo(TaskHandle, &xTaskDetails, pdTRUE, eInvalid);
+	printf1("Task Name: %s \r\n", xTaskDetails.pcTaskName);
+	printf1("Task Priority: %d\r\n", xTaskDetails.uxCurrentPriority);
+	printf1("Base Priority: %d\r\n", xTaskDetails.uxBasePriority);
+	printf1("Task State: %d\r\n", xTaskDetails.eCurrentState);
+	printf1("Stack Base Address: %p\r\n", xTaskDetails.pxStackBase);
+	printf1("Stack High Water Mark: %d\r\n", xTaskDetails.usStackHighWaterMark);
+	printf1("Task Run Time: %d ticks\r\n", xTaskDetails.ulRunTimeCounter);
+	printf1("\r\n");
+}
+
+void task1(void* arg) {
+	while (1) {
+		Delay_ms(1000);
+		Print_Task_Info(hand1);
+	}
+}
+
+int main(void) {
+	USART1_Init();
+	Delay_Init();
+	NVIC_SetPriorityGrouping(NVIC_PriorityGroup_4);
+
+	xTaskCreate(task1, "Task1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, &hand1);
+
+	vTaskStartScheduler();
+
+	while (1) {
+	}
+}
+
+
