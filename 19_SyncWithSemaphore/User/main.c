23f8d4354308059a9673e2bf367b403b9211f551
ambi <forworkingliu@163.com>
2025-07-15 15:31:21 +0800

    使用二值信号量实现task1和task2的互斥执行,多次挑时候才逐渐实现,卡点在临界区的实现在脑海中不明确,对于这个程序而言,谁可以上CPU执行,就是两个任务的共享资源,所以通过二值信号量构建的临界区应该将完整的任务逻辑包含在临界区中,这样才能实现两个任务的互斥执行

---
+++ 19_SyncWithSemaphore/User/main.c
@@ -0,0 +1,77 @@
+#include "stm32f10x.h"    
+#include "freertos.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
+#include "UART1.h" 
+#include <string.h>
+
+SemaphoreHandle_t xBinarySemaphore;
+
+void Devices_Init(void);
+
+void task1(void* arg) {
+
+	while (1) {
+		if (xSemaphoreTake(xBinarySemaphore, NULL) == pdTRUE) {
+			// 控制灯亮
+			GPIO_SetBits(GPIOA, GPIO_Pin_1);
+			vTaskDelay(1000);
+
+			// 控制灯灭
+			GPIO_ResetBits(GPIOA, GPIO_Pin_1);
+			xSemaphoreGive(xBinarySemaphore);
+			vTaskDelay(1000);
+		}
+	}
+}
+
+void task2(void* arg) {
+
+	while (1) {
+		if (xSemaphoreTake(xBinarySemaphore, NULL) == pdTRUE) {
+			GPIO_ResetBits(GPIOA, GPIO_Pin_2);
+			vTaskDelay(1000);
+
+			GPIO_SetBits(GPIOA, GPIO_Pin_2);
+			xSemaphoreGive(xBinarySemaphore);
+			vTaskDelay(1000);
+		}
+	}
+}
+
+
+int main(void) {
+	NVIC_SetPriorityGrouping(NVIC_PriorityGroup_4);
+
+	USART1_Init();
+	Devices_Init();
+
+
+	xTaskCreate(task1, "Task1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+	xTaskCreate(task2, "Task2", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+	xBinarySemaphore = xSemaphoreCreateBinary();
+	xSemaphoreGive(xBinarySemaphore);
+	vTaskStartScheduler();
+
+	while (1) {
+	}
+}
+
+
+void Devices_Init(void) {
+	//开启外设时钟
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+	//初始化PA1PA2PA3
+	GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_2;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
+
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+	//默认关闭LED
+	GPIO_ResetBits(GPIOA, GPIO_Pin_1);
+	//默认关闭Buzzer 
+	GPIO_SetBits(GPIOA, GPIO_Pin_2);
+}
